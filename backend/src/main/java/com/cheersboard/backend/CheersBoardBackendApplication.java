de0b141a007920bcffd2c374a28ec98d01da88ee
ricardooandree <ricardooandree@gmail.com>
2025-06-29 18:08:48 +0100

    init: initialize spring boot project w/ dependencies

---
+++ backend/src/main/java/com/cheersboard/backend/CheersBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cheersboard.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheersBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheersBoardBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cheersboard/backend/CheersBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheersboard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheersBoardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
