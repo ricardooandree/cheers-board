725bd2a35fc25c5434e74844dea1206cd9813f40
ricardooandree <ricardooandree@gmail.com>
2025-07-05 17:21:23 +0100

    update: add new custom name methods

--- backend/src/main/java/com/cheersboard/backend/repository/UserRepository.java
+++ backend/src/main/java/com/cheersboard/backend/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByUsernameIgnoreCase(String username);
     boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
