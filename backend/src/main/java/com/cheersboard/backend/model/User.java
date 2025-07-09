36f9fe770fef3c28dae0bd6ffafebe06c745ef0e
ricardooandree <ricardooandree@gmail.com>
2025-07-09 12:40:54 +0100

    fix: remove not null annotation from createdAt field

--- backend/src/main/java/com/cheersboard/backend/model/User.java
+++ backend/src/main/java/com/cheersboard/backend/model/User.java
@@ -3,7 +3,6 @@ package com.cheersboard.backend.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -34,7 +33,6 @@ public class User {
     @Column(unique = true)
     private String email;
 
-    @NotNull
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
