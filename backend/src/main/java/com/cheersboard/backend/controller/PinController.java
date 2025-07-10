38175bac8a15ff187bda1c6a999e0b515079d6b8
ricardooandree <ricardooandree@gmail.com>
2025-07-10 13:01:08 +0100

    feat: add pin service and controller

---
+++ backend/src/main/java/com/cheersboard/backend/controller/PinController.java
@@ -0,0 +1,79 @@
+package com.cheersboard.backend.controller;
+
+import com.cheersboard.backend.dto.like.LikeResponse;
+import com.cheersboard.backend.dto.pin.CreatePinRequest;
+import com.cheersboard.backend.dto.pin.PinResponse;
+import com.cheersboard.backend.dto.pin.UpdateDescriptionRequest;
+import com.cheersboard.backend.service.PinService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pins")
+public class PinController {
+    private final PinService pinService;
+
+    public PinController(PinService pinService){
+        this.pinService = pinService;
+    }
+
+    /**
+     * CREATE Routes
+     */
+    @PostMapping
+    public ResponseEntity<PinResponse> createPin(@Valid @RequestBody CreatePinRequest createPinRequest){
+        PinResponse createdPin = pinService.createPin(createPinRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPin);
+    }
+
+    /**
+     * GET Routes
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<PinResponse> getPin(@PathVariable("id") Long id){
+        PinResponse pin = pinService.getPinById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(pin);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PinResponse>> getPins(){
+        List<PinResponse> pins = pinService.getAllPins();
+
+        return ResponseEntity.status(HttpStatus.OK).body(pins);
+    }
+
+    /**
+     * UPDATE Routes
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<PinResponse> updatePin(@PathVariable("id") Long id,
+                                                 @Valid @RequestBody UpdateDescriptionRequest updateDescriptionRequest){
+        PinResponse updatedPin = pinService.updatePinDescription(id, updateDescriptionRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedPin);
+    }
+
+    /**
+     * DELETE Routes
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PinResponse> deletePin(@PathVariable("id") Long id){
+        pinService.deletePinById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping("/{id}/likes")
+    public ResponseEntity<List<LikeResponse>> getPinLikes(@PathVariable("id") Long id){
+        List<LikeResponse> likes = pinService.getPinLikes(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(likes);
+    }
+
+}
--- backend/src/main/java/com/cheersboard/backend/service/PinService.java
+++ backend/src/main/java/com/cheersboard/backend/service/PinService.java
@@ -1,55 +1,114 @@
 package com.cheersboard.backend.service;
 
+import com.cheersboard.backend.dto.like.LikeResponse;
+import com.cheersboard.backend.dto.pin.CreatePinRequest;
+import com.cheersboard.backend.dto.pin.PinResponse;
+import com.cheersboard.backend.dto.pin.UpdateDescriptionRequest;
 import com.cheersboard.backend.exception.ResourceNotFoundException;
+import com.cheersboard.backend.model.Location;
 import com.cheersboard.backend.model.Pin;
 import com.cheersboard.backend.model.User;
+import com.cheersboard.backend.repository.LocationRepository;
 import com.cheersboard.backend.repository.PinRepository;
 import com.cheersboard.backend.repository.UserRepository;
+import com.cheersboard.backend.util.mapper.LikeMapper;
+import com.cheersboard.backend.util.mapper.PinMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PinService {
     private final PinRepository pinRepository;
     private final UserRepository userRepository;
+    private final LocationService locationService;
+    private final PinMapper pinMapper;
+    private final LikeMapper likeMapper;
+    private final LocationRepository locationRepository;
 
-    public PinService(PinRepository pinRepository, UserRepository userRepository) {
+    public PinService(PinRepository pinRepository,
+                      UserRepository userRepository,
+                      LocationService locationService,
+                      PinMapper pinMapper,
+                      LikeMapper likeMapper, LocationRepository locationRepository) {
         this.pinRepository = pinRepository;
         this.userRepository = userRepository;
+        this.locationService = locationService;
+        this.pinMapper = pinMapper;
+        this.likeMapper = likeMapper;
+        this.locationRepository = locationRepository;
     }
 
-    // CRUD Service Methods
-    public Pin createPin(Pin pin, Long userId){
-        User user = userRepository.findById(userId)
+    /**
+     * CREATE Methods
+     */
+    public PinResponse createPin(CreatePinRequest createPinRequest){
+        // TODO: Remove userId from CreatePinRequest DTO after JWT implementation
+        User user = userRepository.findById(createPinRequest.getUserId())
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        user.addPin(pin);
-        return pinRepository.save(pin);
+        // TODO: Implement resolveOrCreate service method
+        // Location location = locationService.resolveOrCreate(createPinRequest.getLatitude(), createPinRequest.getLatitude());
+        // Location location = locationRepository.findByLatitudeAndLongitude(createPinRequest.getLatitude(), createPinRequest.getLatitude())
+        //                .orElse(create?)
+        // Something like that ^^^
+
+        Pin newPin = pinMapper.toEntity(createPinRequest, user, location);
+
+        user.addPin(newPin);
+        location.addPin(newPin);
+        pinRepository.save(newPin);
+
+        return pinMapper.toResponse(newPin);
     }
 
-    public Pin getPinById(Long id){
-        return pinRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Pin not found"));
-    }
-
-    public List<Pin> getAllPins(){
-        return pinRepository.findAll();
-    }
-
-    public Pin updatePinDescription(Long id, String newDescription){
+    /**
+     * GET Methods
+     */
+    public PinResponse getPinById(Long id){
         Pin pin = pinRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Pin not found"));
 
-        pin.setDescription(newDescription);
-        return pinRepository.save(pin);
+        return pinMapper.toResponse(pin);
     }
 
+    public List<PinResponse> getAllPins(){
+        List<Pin> pins = pinRepository.findAll();
+
+        return pinMapper.toResponseList(pins);
+    }
+
+    public List<LikeResponse> getPinLikes(Long id){
+        Pin pin = pinRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Pin not found"));
+
+        return pin.getLikes().stream()
+                .map(likeMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * UPDATE Methods
+     */
+    public PinResponse updatePinDescription(Long id, UpdateDescriptionRequest updateDescriptionRequest){
+        Pin pin = pinRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Pin not found"));
+
+        pin.setDescription(updateDescriptionRequest.getDescription());
+        pinRepository.save(pin);
+
+        return pinMapper.toResponse(pin);
+    }
+
+    /**
+     * DELETE Methods
+     */
     public void deletePinById(Long id){
-        Pin pin = pinRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Pin not found"));
+        if (!pinRepository.existsById(id)){
+            throw new ResourceNotFoundException("Pin not found");
+        }
 
-        pin.getUser().removePin(pin);
-        pinRepository.delete(pin);
+        pinRepository.deleteById(id);
     }
 }
