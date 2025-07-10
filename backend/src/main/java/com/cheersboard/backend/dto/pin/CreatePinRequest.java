e62dca7b75522e98dec77f703fe816fd3c5b1409
ricardooandree <ricardooandree@gmail.com>
2025-07-10 13:07:35 +0100

    fix: change create pin request dto to not include entity objects

--- backend/src/main/java/com/cheersboard/backend/dto/pin/CreatePinRequest.java
+++ backend/src/main/java/com/cheersboard/backend/dto/pin/CreatePinRequest.java
@@ -2,10 +2,16 @@ package com.cheersboard.backend.dto.pin;
 
 import com.cheersboard.backend.model.Location;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class CreatePinRequest {
+    // TODO: Remove userId once JWT is integrated
     private Long userId;
-    private Location location;
+    private Double latitude;
+    private Double longitude;
     private String description;
 }
