11d898031f0ff34b77f280e4c0b76ba744943663
ricardooandree <ricardooandree@gmail.com>
2025-07-12 16:46:19 +0100

    fix: update find by latitude and longitude repository name method return type

--- backend/src/main/java/com/cheersboard/backend/repository/LocationRepository.java
+++ backend/src/main/java/com/cheersboard/backend/repository/LocationRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-    List<Location> findByLatitudeAndLongitude(Double latitude, Double longitude);
+    Optional<Location> findByLatitudeAndLongitude(Double latitude, Double longitude);
     List<Location> findByAddress(String address);
     List<Location> findByCity(String city);
     List<Location> findByCountry(String country);
